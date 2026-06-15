4f4d7e94f0608bc6a39a39fa4a67f949b55ad756
Jose Principe <lombard.ff18@gmail.com>
2026-06-14 21:57:34 -0500

    proyecto de innovación

---
+++ main.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+
+from interfaz import SistemaFragua
+
+root = ctk.CTk()
+
+app = SistemaFragua(root)
+
+root.mainloop()
---
+++ materiales.py
@@ -0,0 +1,82 @@
+# materiales.py
+
+materiales = {
+    # ===================================================
+    # 1. METALES A FRAGUAR (Se muestran en el primer menú)
+    # ===================================================
+    "Acero": {
+        "temp": 1150,
+        "calor_especifico": 0.49,
+        "densidad": 7850
+    },
+    "Hierro": {
+        "temp": 1200,
+        "calor_especifico": 0.45,
+        "densidad": 7874
+    },
+    "Aluminio": {
+        "temp": 450,
+        "calor_especifico": 0.90,
+        "densidad": 2700
+    },
+    "Cobre": {
+        "temp": 850,
+        "calor_especifico": 0.39,
+        "densidad": 8960
+    },
+    "Acero Inoxidable (304)": {
+        "temp": 1200,
+        "calor_especifico": 0.50,
+        "densidad": 8000
+    },
+    "Titanio (Grado 5)": {
+        "temp": 950,
+        "calor_especifico": 0.52,
+        "densidad": 4430
+    },
+    # --- Los 5 metales agregados aquí ---
+    "Acero de Herramientas (H13)": {
+        "temp": 1120,
+        "calor_especifico": 0.46,
+        "densidad": 7800
+    },
+    "Latón (C360)": {
+        "temp": 750,
+        "calor_especifico": 0.38,
+        "densidad": 8500
+    },
+    "Bronce": {
+        "temp": 820,
+        "calor_especifico": 0.38,
+        "densidad": 8800
+    },
+    "Inconel 718 (Superaleación)": {
+        "temp": 1140,
+        "calor_especifico": 0.43,
+        "densidad": 8190
+    },
+    "Magnesio": {
+        "temp": 400,
+        "calor_especifico": 1.02,
+        "densidad": 1740
+    },
+
+    # ===================================================
+    # 2. MATERIALES DE PARED (Se muestran en el segundo menú)
+    # ===================================================
+    "PARED: Ladrillo Refractario Alta Alúmina": {
+        "temp": 0,
+        "calor_especifico": 1.05,
+        "densidad": 2300
+    },
+    "PARED: Fibra Cerámica Aislante": {
+        "temp": 0,
+        "calor_especifico": 1.13,
+        "densidad": 128
+    },
+    "PARED: Ladrillo de Magnesita": {
+        "temp": 0,
+        "calor_especifico": 1.15,
+        "densidad": 2850
+    }
+}
